e38781df0994b0d4294c3751f63e81526f84b37e
pavelkharitenko <pavel.kharitenko@googlemail.com>
2021-10-15 17:51:37 +0200

    updated solverdummy

--- solverdummy/solverdummy-parallel-mpi/solverdummy-parallel-mpi.jl
+++ solverdummy/solverdummy-parallel-mpi/solverdummy-parallel-mpi.jl
@@ -4,41 +4,43 @@ using PreCICE
 using MPI
 
 
-if size(ARGS, 1) < 3
-    println("ERROR: pass config path, solver name and mesh name, example: julia solverdummy.jl ./precice-config.xml SolverOne MeshOne")
+if size(ARGS, 1) < 2
+    println("ERROR: pass config path, solver name, 
+        example: julia solverdummy.jl ./precice-config.xml SolverOne")
     exit(1)
 end
 
 MPI.Init()
 comm = MPI.COMM_WORLD
 
-
 commRank = MPI.Comm_rank(comm)
 commSize = MPI.Comm_size(comm)
 
-
 configFileName = ARGS[1]
 solverName = ARGS[2]
-meshName = ARGS[3]
 
-#println("""DUMMY ($(MPI.Comm_rank(comm))): Running solver dummy with preCICE config file "$configFileName", participant name "$solverName", and mesh name "$meshName" """)
+# set meshName depending on solverName
+if solverName == "SolverOne"
+    meshName = "MeshOne"
+else 
+    meshName = "MeshTwo"
+end
+
+println("""DUMMY ($(MPI.Comm_rank(comm))): Running solver dummy with preCICE config file 
+        "$configFileName", participant name "$solverName", and mesh name "$meshName" """)
 
 PreCICE.createSolverInterface(solverName, configFileName, commRank, commSize)
 
 meshID = PreCICE.getMeshID(meshName)
 dimensions = PreCICE.getDimensions()
 
-dataWriteName = nothing
-dataReadName = nothing
 
 numberOfVertices = 1
 
 if solverName == "SolverOne"
     dataWriteName = "dataOne"
     dataReadName  = "dataTwo"
-end
-
-if solverName == "SolverTwo"
+else
     dataReadName  = "dataOne"
     dataWriteName = "dataTwo"
 end
@@ -46,17 +48,15 @@ end
 readDataID  = PreCICE.getDataID(dataReadName, meshID)
 writeDataID = PreCICE.getDataID(dataWriteName, meshID)
 
-readData = Array{Float64, 1}(undef, numberOfVertices * dimensions)
-writeData = Array{Float64, 1}(undef, numberOfVertices * dimensions)
+readData = zeros(numberOfVertices * dimensions)
+writeData = zeros(numberOfVertices * dimensions)
+
 vertices = Array{Float64, 1}(undef, numberOfVertices * dimensions)
 
-
-for i in 1:numberOfVertices
-    for j in 1:dimensions
-        vertices[j+ dimensions * (i-1)] = i-1 + commRank 
-        readData[j+ dimensions * (i-1)] = i-1 + commRank 
-        writeData[j+ dimensions * (i-1)] = i-1 + commRank
-    end
+# create different vertices coordinates for different procs
+for i in 1:numberOfVertices, j in 1:dimensions
+    offset = commRank * numberOfVertices
+    vertices[j + dimensions * (i-1)] = i-1 + offset
 end
 
 vertexIDs = PreCICE.setMeshVertices(meshID, numberOfVertices, vertices)
@@ -87,18 +87,17 @@ while PreCICE.isCouplingOngoing()
     dt = PreCICE.advance(dt)
 
     if PreCICE.isActionRequired(PreCICE.actionReadIterationCheckpoint())
-        #println("""DUMMY ($(MPI.Comm_rank(comm))): Reading iteration checkpoint""")
+        println("""DUMMY ($(MPI.Comm_rank(comm))): Reading iteration checkpoint""")
         PreCICE.markActionFulfilled(PreCICE.actionReadIterationCheckpoint())
     else
-        #println("""DUMMY ($(MPI.Comm_rank(comm))): Advancing in time""")
+        println("""DUMMY ($(MPI.Comm_rank(comm))): Advancing in time""")
     end
 
 end # while
 
-end # let scope
+end # let 
 
 PreCICE.finalize()
-
 
 println("""DUMMY ($(MPI.Comm_rank(comm))): Closing Julia solver dummy...""")
 
