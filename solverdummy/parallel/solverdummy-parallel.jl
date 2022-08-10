3b709bb7f3d4c0a93069c74e908d172dd7edf268
Erik Scheurer <erik.scheurer@gmx.de>
2022-08-10 11:05:52 +0200

    Port parallel dummy to new bindings

--- solverdummy/parallel/solverdummy-parallel.jl
+++ solverdummy/parallel/solverdummy-parallel.jl
@@ -1,8 +1,7 @@
-import Pkg; Pkg.activate("../..")
 using Distributed
 
 if size(ARGS, 1) < 3
-    println("ERROR: pass total processes number N, config path, solver name and mesh name, 
+    println("ERROR: pass total processes number N, config path and solver name, 
                 example: julia solverdummy.jl 5 ./precice-config.xml SolverOne")
     exit(1)
 end
@@ -24,11 +23,14 @@ commSize = nprocs()
 configFileName = $ConfigFileName
 solverName = $SolverName
 
-# set meshName depending on solverName
 if solverName == "SolverOne"
     meshName = "MeshOne"
-else 
+    dataWriteName = "dataOne"
+    dataReadName = "dataTwo"
+else
     meshName = "MeshTwo"
+    dataReadName = "dataOne"
+    dataWriteName = "dataTwo"
 end
 
 println("""DUMMY ($commRank): Running solver dummy with preCICE config file "$configFileName", 
@@ -39,78 +41,59 @@ PreCICE.createSolverInterface(solverName, configFileName, commRank, commSize)
 meshID = PreCICE.getMeshID(meshName)
 dimensions = PreCICE.getDimensions()
 
-dataWriteName = nothing
-dataReadName = nothing
 
-numberOfVertices = 1
+numberOfVertices = 3
 
 
-if solverName == "SolverOne"
-    dataWriteName = "dataOne"
-    dataReadName  = "dataTwo"
-end
-
-if solverName == "SolverTwo"
-    dataReadName  = "dataOne"
-    dataWriteName = "dataTwo"
-end
-
-readDataID  = PreCICE.getDataID(dataReadName, meshID)
+readDataID = PreCICE.getDataID(dataReadName, meshID)
 writeDataID = PreCICE.getDataID(dataWriteName, meshID)
 
+writeData = zeros(numberOfVertices, dimensions)
 
-writeData = zeros(numberOfVertices * dimensions)
-readData = zeros(numberOfVertices * dimensions)
+vertices = Array{Float64,2}(undef, numberOfVertices, dimensions)
 
-vertices = Array{Float64, 1}(undef, numberOfVertices * dimensions)
-
-# create different vertices coordinates for different procs
-for i in 1:numberOfVertices, j in 1:dimensions
-    offset = commRank * numberOfVertices
-    vertices[j + dimensions * (i-1)] = i-1 + offset
+for i = 1:numberOfVertices, j = 1:dimensions
+    vertices[i, j] = i
 end
 
-
-
-
-println(vertices)
+vertexIDs = PreCICE.setMeshVertices(meshID, vertices)
 
 let # setting local scope for dt outside of the while loop
-vertexIDs = PreCICE.setMeshVertices(meshID, numberOfVertices, vertices) 
 
-dt = PreCICE.initialize()
+    dt = PreCICE.initialize()
+    readData = zeros(numberOfVertices, dimensions)
 
-while PreCICE.isCouplingOngoing()
-    
-    if PreCICE.isActionRequired(PreCICE.actionWriteIterationCheckpoint())
-        println("DUMMY ($commRank): Writing iteration checkpoint")
-        PreCICE.markActionFulfilled(PreCICE.actionWriteIterationCheckpoint())
-    end
+    while PreCICE.isCouplingOngoing()
 
-    if PreCICE.isReadDataAvailable()
-        PreCICE.readBlockVectorData(readDataID, numberOfVertices, vertexIDs, readData) 
-    end
+        if PreCICE.isActionRequired(PreCICE.actionWriteIterationCheckpoint())
+            println("DUMMY ($commRank): Writing iteration checkpoint")
+            PreCICE.markActionFulfilled(PreCICE.actionWriteIterationCheckpoint())
+        end
 
-    writeData = readData .+ 1.0
+        if PreCICE.isReadDataAvailable()
+            readData = PreCICE.readBlockVectorData(readDataID, vertexIDs)
+        end
 
-    if PreCICE.isWriteDataRequired(dt)
-        PreCICE.writeBlockVectorData(writeDataID, numberOfVertices, vertexIDs, writeData)
-    end
+        writeData = readData .+ 1.0
 
-    dt = PreCICE.advance(dt)
+        if PreCICE.isWriteDataRequired(dt)
+            PreCICE.writeBlockVectorData(writeDataID, vertexIDs, writeData)
+        end
 
-    if PreCICE.isActionRequired(PreCICE.actionReadIterationCheckpoint())
-        println("DUMMY ($commRank): Reading iteration checkpoint")
-        PreCICE.markActionFulfilled(PreCICE.actionReadIterationCheckpoint())
-    else
-       println("DUMMY ($commRank): Advancing in time")
-    end
+        dt = PreCICE.advance(dt)
 
-end # while
+        if PreCICE.isActionRequired(PreCICE.actionReadIterationCheckpoint())
+            println("DUMMY ($commRank): Reading iteration checkpoint")
+            PreCICE.markActionFulfilled(PreCICE.actionReadIterationCheckpoint())
+        else
+            println("DUMMY ($commRank): Advancing in time")
+        end
+
+    end # while
 
 end # let scope
 
 PreCICE.finalize()
 println("DUMMY ($commRank): Closing Julia solver dummy...")
 
-end
+end # begin
