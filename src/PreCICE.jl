d5ca26c6530f8769acb8557c2456ccabe27e53f1
Erik Scheurer <erik.scheurer@gmx.de>
2022-09-24 13:25:18 +0200

    Update documentation and remove size variable

--- src/PreCICE.jl
+++ src/PreCICE.jl
@@ -1360,6 +1360,9 @@ end
 
 Write gradient data of a vector data as a block, the value of a specified vertices to a dataID.
 
+The format for a 2D problem with 2 vertices is [v1x_dx v1y_dx v1x_dy v1y_dy; v2x_dx v2y_dx v2x_dy v2y_dy]
+The format for a 3D problem with 2 vertices is [v1x_dx v1y_dx v1z_dx v1x_dy v1y_dy v1z_dy v1x_dz v1y_dz v1z_dz; v2x_dx v2y_dx v2z_dx v2x_dy v2y_dy v2z_dy v2x_dz v2y_dz v2z_dz]
+
 # Arguments
 - `dataID::Integer`: ID of the data to be written. Obtained by [`getDataID`](@ref).
 - `valueIndices::AbstractArray{Cint}`: Indices of the vertex.
@@ -1395,6 +1398,12 @@ end
 
 Write gradient data of a scalar data, the value of a specified vertex to a dataID.
 
+The 2D-format of gradientValues is [v_dx, v_dy] vector corresponding to the data block v = [v]
+differentiated respectively in x-direction dx and y-direction dy
+
+The 3D-format of gradientValues is [v_dx, v_dy, v_dz] vector
+corresponding to the data block v = [v] differentiated respectively in spatial directions x-direction dx and y-direction dy and z-direction dz
+
 # Arguments
 - `dataID::Integer`: ID of the data to be written. Obtained by [`getDataID`](@ref).
 - `valueIndex::Integer`: Indice of the vertex.
@@ -1418,7 +1427,7 @@ writeScalarGradientData(data_id, vertex_id, gradientValue)
 
 """
 function writeScalarGradientData(dataID::Integer, valueIndex::Integer, gradientValues::AbstractArray{Float64})
-    _size, dimensions = size(gradientValues)
+    dimensions = size(gradientValues)
     @assert dimensions == getDimensions() "Dimensions of vector data in write_scalar_gradient_data does not match with dimensions in problem definition. Provided dimensions: $dimensions, expected dimensions: $(getDimensions())"
    
     ccall((:precicec_writeScalarGradientData, "libprecice"), Cvoid, (Cint, Cint, Ref{Cdouble}), dataID, valueIndex, gradientValues)
@@ -1430,6 +1439,12 @@ end
     writeVectorGradientData(dataID::Integer, valueIndex::Integer, gradientValues::AbstractArray{Float64})
 
 Write gradient data of a vector data, the value of a specified vertex to a dataID.
+
+The 2D-format of gradientValues is [vx_dx, vy_dx, vx_dy, vy_dy] vector corresponding to the data block v = [vx, vy]
+differentiated respectively in x-direction dx and y-direction dy
+
+The 3D-format of gradientValues is [vx_dx, vy_dx, vz_dx, vx_dy, vy_dy, vz_dy, vx_dz, vy_dz, vz_dz] vector
+corresponding to the data block v = [vx, vy, vz] differentiated respectively in spatial directions x-direction dx and y-direction dy and z-direction dz
 
 # Arguments
 - `dataID::Integer`: ID of the data to be written. Obtained by [`getDataID`](@ref).
@@ -1453,7 +1468,7 @@ writeVectorGradientData(data_id, vertex_id, gradientValue)
 ```
 """
 function writeVectorGradientData(dataID::Integer, valueIndex::Integer, gradientValues::AbstractArray{Float64})
-    _size, dimensions = size(gradientValues)
+    dimensions = size(gradientValues)
     @assert dimensions == getDimensions()*getDimensions() "Dimensions of vector data in write_vector_gradient_data does not match with dimensions in problem definition. Provided dimensions: $dimensions, expected dimensions: $(getDimensions()*getDimensions())"
 
     ccall((:precicec_writeVectorGradientData, "libprecice"), Cvoid, (Cint, Cint, Ref{Cdouble}), dataID, valueIndex, gradientValues)
@@ -1469,7 +1484,7 @@ Write gradient data of a scalar data as a block, the value of a specified vertic
 # Arguments
 - `dataID::Integer`: ID of the data to be written. Obtained by [`getDataID`](@ref).
 - `valueIndices::AbstractArray{Cint}`: Indices of the vertex.
-- `gradientValues::AbstractArray{Float64}`: The gradient values to write.
+- `gradientValues::AbstractArray{Float64}`: The gradient values to write. For example for a 2D problem use the format [v1_dx v1_dy; v2_dx v2_dy]
 
 # Notes
 
