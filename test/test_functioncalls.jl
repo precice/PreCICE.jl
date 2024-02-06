ad92086acf3cbc3d2eebccaee3aedabdf8325175
Ishaan Desai <ishaandesai@gmail.com>
2024-02-06 13:08:50 +0100

    Rename DummySolverInterface.c to DummyParticipant.c in test/

--- test/test_functioncalls.jl
+++ test/test_functioncalls.jl
@@ -13,16 +13,16 @@ end
 
 function getMeshDimensions()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshDimension = 3  # compare to test/SolverInterface.c, fake_dimensions
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
+    fakeMeshDimension = 3  # compare to test/Participant.c, fake_dimensions
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
     return fakeMeshDimension == PreCICE.getMeshDimensions(fakeMeshName)
 end
 
 function getDataDimensions()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeDataName = "FakeData"  # compare to test/SolverInterface.c, fake_data_name
-    fakeDataDimension = 3  # compare to test/SolverInterface.c, fake_dimensions
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeDataName = "FakeData"  # compare to test/Participant.c, fake_data_name
+    fakeDataDimension = 3  # compare to test/Participant.c, fake_dimensions
     return fakeDataDimension == PreCICE.getDataDimensions(fakeMeshName, fakeDataName)
 end
 
@@ -58,27 +58,27 @@ end
 
 function hasMesh()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
     return false == PreCICE.hasMesh(fakeMeshName)
 end
 
 function hasData()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeDataName = "FakeData"  # compare to test/SolverInterface.c, fake_data_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeDataName = "FakeData"  # compare to test/Participant.c, fake_data_name
     return false == PreCICE.hasData(fakeMeshName, fakeDataName)
 end
 
 function requiresMeshConnectivityFor()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
     return false == PreCICE.requiresMeshConnectivityFor(fakeMeshName)
 end
 
 function setMeshVertex()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeMeshDimension = 3  # compare to test/SolverInterface.c, fake_dimensions
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeMeshDimension = 3  # compare to test/Participant.c, fake_dimensions
     fakePosition = rand(Cdouble, fakeMeshDimension)
     fakeVertexId = PreCICE.setMeshVertex(fakeMeshName, fakePosition)
     return 0 == fakeVertexId
@@ -87,9 +87,9 @@ end
 
 function setMeshVertices()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeMeshDimension = 3  # compare to test/SolverInterface.c, fake_dimensions
-    nFakeVertices = 3  # compare to test/SolverInterface.c, n_fake_vertices
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeMeshDimension = 3  # compare to test/Participant.c, fake_dimensions
+    nFakeVertices = 3  # compare to test/Participant.c, n_fake_vertices
     positions = rand(Cdouble, (nFakeVertices, fakeMeshDimension))
     expectedOutput = 0:(nFakeVertices-1)
     actualOutput = PreCICE.setMeshVertices(fakeMeshName, positions)
@@ -98,9 +98,9 @@ end
 
 # function setMeshVerticesEmpty()
 #     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-#     fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-#     fakeMeshDimension = 3  # compare to test/SolverInterface.c, fake_dimensions
-#     nFakeVertices = 0  # compare to test/SolverInterface.c, n_fake_vertices
+#     fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+#     fakeMeshDimension = 3  # compare to test/Participant.c, fake_dimensions
+#     nFakeVertices = 0  # compare to test/Participant.c, n_fake_vertices
 #     positions = rand(Cdouble, (nFakeVertices, fakeMeshDimension))
 #     expectedOutput = []
 #     actualOutput = PreCICE.setMeshVertices(fakeMeshName, positions)
@@ -109,8 +109,8 @@ end
 
 function getMeshVertexSize()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    nFakeVertices = 3  # compare to test/SolverInterface.c, n_fake_vertices
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    nFakeVertices = 3  # compare to test/Participant.c, n_fake_vertices
     nVertices = PreCICE.getMeshVertexSize(fakeMeshName)
     return nFakeVertices == nVertices
 end
@@ -126,8 +126,8 @@ end
 
 function readWriteData()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeDataName = "FakeData"  # compare to test/SolverInterface.c, fake_data_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeDataName = "FakeData"  # compare to test/Participant.c, fake_data_name
     writeData = [3.0 7.0 8.0; 7.0 6.0 5.0]
     PreCICE.writeData(fakeMeshName, fakeDataName, Cint[1, 2], writeData)
     readData = PreCICE.readData(fakeMeshName, fakeDataName, Cint[1, 2], 1.0)
@@ -144,16 +144,16 @@ end
 
 function requiresGradientDataFor()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeDataName = "FakeData"  # compare to test/SolverInterface.c, fake_data_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeDataName = "FakeData"  # compare to test/Participant.c, fake_data_name
     return false == PreCICE.requiresGradientDataFor(fakeMeshName, fakeDataName)
 
 end
 
 function writeGradientData()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeDataName = "FakeData"  # compare to test/SolverInterface.c, fake_data_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeDataName = "FakeData"  # compare to test/Participant.c, fake_data_name
     nVertices = 2
     ndims = 3
     gradientData = rand(nVertices, ndims * ndims)
@@ -166,13 +166,13 @@ end
 
 function getVersionInformation()
     versionInfo = PreCICE.getVersionInformation()
-    fakeVersionInfo = "dummy"  # compare to test/SolverInterface.c
+    fakeVersionInfo = "dummy"  # compare to test/Participant.c
     return versionInfo == fakeVersionInfo
 end
 
 function setMeshAccessRegion()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
     fakeBoundingBox = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0]
     PreCICE.setMeshAccessRegion(fakeMeshName, fakeBoundingBox)
     return true
@@ -180,9 +180,9 @@ end
 
 function getMeshVerticesAndIDs()
     PreCICE.createParticipant("test", "dummy.xml", 0, 1)
-    fakeMeshName = "FakeMesh"  # compare to test/SolverInterface.c, fake_mesh_name
-    fakeMeshDimension = 3  # compare to test/SolverInterface.c, fake_dimensions
-    nFakeVertices = 3 # compare to test/SolverInterface.c, fake_n_vertices
+    fakeMeshName = "FakeMesh"  # compare to test/Participant.c, fake_mesh_name
+    fakeMeshDimension = 3  # compare to test/Participant.c, fake_dimensions
+    nFakeVertices = 3 # compare to test/Participant.c, fake_n_vertices
     vertexIDs = Cint[0, 1, 2]
     expected_vertices =
         reshape(0:(nFakeVertices*fakeMeshDimension-1), (nFakeVertices, fakeMeshDimension))
